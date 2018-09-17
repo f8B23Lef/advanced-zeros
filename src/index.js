22e6824332624f32e8ae88833ab54a4c52569f64
f8B23Lef <mal7rish@yandex.ru>
2018-09-17 22:59:32 +0300

    advanced-zeros/implement function which calculate trailing zeros of the factorial in different base sistems

--- src/index.js
+++ src/index.js
@@ -1,3 +1,64 @@
 module.exports = function getZerosCount(number, base) {
-  // your implementation
+
+  var primeFactors = decomposePrimeFactors(base);
+  var zeros = -1;
+
+  for(var i = 0; i < primeFactors.length; i++) {
+    var base = primeFactors[i][0];
+    var exp = primeFactors[i][1];
+    var countFactor = Math.trunc(calcCountPrimeFactor(number, base) / exp);
+    if(zeros == -1 || countFactor < zeros) {
+      zeros = countFactor;
+    }
+  }
+
+  return zeros;
+}
+  
+function decomposePrimeFactors(num) {
+    
+  var primeFactors = [];  
+    
+  for(var i = 2; i <= num; i++) {
+    while(num % i === 0) {
+      primeFactors.push(i);
+      num = num / i;
+     }
+  }
+   
+  return formPowerFactorPairs(primeFactors);
+}
+  
+function formPowerFactorPairs(primeFactors) {
+  
+  var powerFactors = [];
+  var prevFactor = primeFactors[0];
+  var count = 1;
+    
+  for(var i = 1; i < primeFactors.length; i++) {
+  
+    if(primeFactors[i] === prevFactor) {
+      count++;
+    } else {
+      powerFactors.push([prevFactor, count]);
+      prevFactor = primeFactors[i];
+      count = 1;
+    }
+      
+  }
+    
+  powerFactors.push([prevFactor, count]);
+  
+  return powerFactors;
+}
+
+function calcCountPrimeFactor(number, primeFactor) {
+
+  var countFactor = 0;
+  
+  for (var i = primeFactor; i <= number; i *= primeFactor) {
+    countFactor += Math.trunc(number / i); 
+  }
+
+  return countFactor;
 }
